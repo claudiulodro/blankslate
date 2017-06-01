4855bd303daf6ef8a3d0daf3450cb7bfa33dbbed
claudiulodro <claudiulodro@gmail.com>
2017-05-31 20:57:47 -0700

    Setup assets

--- functions.php
+++ functions.php
@@ -64,11 +64,7 @@ function blankslate_custom_pings( $comment )
 add_filter( 'get_comments_number', 'blankslate_comments_number' );
 function blankslate_comments_number( $count )
 {
-	if ( !is_admin() ) {
-		global $id;
-		$comments_by_type = &separate_comments( get_comments( 'status=approve&post_id=' . $id ) );
-		return count( $comments_by_type['comment'] );
-	} else {
-		return $count;
-	}
+	return $count;
 }
+
+require_once( get_template_directory() . '/lib/include.php' );
---
+++ lib/assets.php
@@ -0,0 +1,6 @@
+<?php
+
+function multithreaded_style() {
+	wp_enqueue_style( 'multithreaded', get_template_directory_uri() . '/assets/multithreaded.css' );
+}
+add_action( 'wp_enqueue_scripts', 'multithreaded_style' );
