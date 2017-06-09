c502e1148ba6e625002cc8622edf10c73f806177
claudiulodro <claudiulodro@gmail.com>
2017-06-08 21:52:15 -0700

    Remove comments

--- entry-summary.php
+++ entry-summary.php
@@ -1,7 +1,7 @@
 <section class="entry-summary">
 	<?php the_excerpt(); ?>
-	<a href="<?php comments_link() ?>">
+	<?php /*<a href="<?php comments_link() ?>">
 		<div class="comments-title"><?php comments_number(); ?></div>
-	</a>
+	</a> */ ?>
 	<?php if( is_search() ) { ?><div class="entry-links"><?php wp_link_pages(); ?></div><?php } ?>
 </section>
--- single.php
+++ single.php
@@ -2,7 +2,7 @@
 <section id="content" role="main">
 	<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
 		<?php get_template_part( 'entry' ); ?>
-		<?php if ( ! post_password_required() ) comments_template( '', true ); ?>
+		<?php /*if ( ! post_password_required() ) comments_template( '', true );*/ ?>
 	<?php endwhile; endif; ?>
 </section>
 <?php get_sidebar(); ?>
